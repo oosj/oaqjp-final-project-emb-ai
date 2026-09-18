67b48f8792250632ea3c501315defe4b39087ed1
Omar Juárez <oosj@live.com>
2026-09-18 09:52:28 -0600

    Cambios solicitados hasta la tarea 5.

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    text = request.args.get('textToAnalyze')
+    response = emotion_detector(text)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is not None:
+        return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return "Invalid input! Try again."
+
+@app.route("/")
+def render_index_page():
+    
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

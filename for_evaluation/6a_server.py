b096b7a4adf9f54891ecb6fe940756a9d8b2e35c
Omar Juárez <oosj@live.com>
2026-09-18 10:12:14 -0600

    Cambios solicitados por IBM hasta la tarea 8.

--- for_evaluation/6a_server.py
+++ for_evaluation/6a_server.py
@@ -1,7 +1,3 @@
-''' Executing this function initiates the application of emotion
-    detection to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
 from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
@@ -10,10 +6,6 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_detector():
-    ''' This code receives the text from the HTML interface and 
-        runs emotion detection over it using emotion_detector()
-        function.
-    '''
     text = request.args.get('textToAnalyze')
     response = emotion_detector(text)
 
@@ -30,9 +22,7 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    
     return render_template('index.html')
 
 if __name__ == "__main__":
